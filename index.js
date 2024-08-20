432b5da676f93b0bee5f59e49a7e067228877060
Aarav Sharma <iamcoder18@gmail.com>
2024-08-20 14:09:02 -0600

    Updated colors

--- index.js
+++ index.js
@@ -1,36 +1,69 @@
 class AsyncExtension {
     getInfo() {
-      return {
-        id: 'launchpadai',
-        name: 'Launchpad AI',
-        color1: '#ff0000', // pure red
-        color2: '#00ff00', // pure green
-        color3: '#0000ff',
-        blocks: [
-          {
-            opcode: 'fetch',
-            text: 'Ask AI [URL]',
-            blockType: Scratch.BlockType.REPORTER,
-            arguments: {
-              URL: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: 'Hi!'
-              }
+        return {
+            id: 'launchpadai',
+            name: 'Launchpad AI',
+            color1: '#d40f30',
+            color2: '#8f0b21',
+            color3: '#d40f30',
+            blocks: [
+                {
+                    opcode: 'fetch',
+                    text: 'Ask AI [PROMPT]',
+                    blockType: Scratch.BlockType.REPORTER,
+                    arguments: {
+                        PROMPT: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'Hi!'
+                        }
+                    }
+                },
+                {
+                    opcode: 'fetchwm',
+                    text: 'Ask [MODEL] [PROMPT]',
+                    blockType: Scratch.BlockType.REPORTER,
+                    arguments: {
+                        PROMPT: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'Hi!'
+                        },
+                        MODEL: {
+                            type: Scratch.ArgumentType.STRING,
+                            menu: "MODEL_MENU"
+                        }
+                    }
+                }
+            ],
+            menus: {
+                MODEL_MENU: {
+                    acceptReporters: true,
+                    items: ['gpt-4o', 'gpt-4o-mini', 'gpt-4-turbo', 'gpt-3.5-turbo']
+                }
             }
-          }
-        ]
-      };
+        };
     }
-  
-    fetch (args) {
-      return fetch("https://launchpad-ai-v2.onrender.com/?prompt=" + args.URL)
-        .then((response) => {
-          return response.text();
-        })
-        .catch((error) => {
-          console.error(error);
-          return 'Uh oh! Something went wrong.';
-        });
+
+    fetch(args) {
+        return fetch("https://launchpad-ai-v2.onrender.com/?prompt=" + args.PROMPT)
+            .then((response) => {
+                return response.text();
+            })
+            .catch((error) => {
+                console.error(error);
+                return 'Uh oh! Something went wrong.';
+            });
     }
-  }
-  Scratch.extensions.register(new AsyncExtension());
+
+    fetchwm(args) {
+        // return fetch(`https://launchpad-ai-v2.onrender.com/wm?model=${args.MODEL}&prompt=${args.PROMPT}`)
+        return fetch(`http://127.0.0.1:5000/wm?model=${args.MODEL}&prompt=${args.PROMPT}`)
+            .then((response) => {
+                return response.text();
+            })
+            .catch((error) => {
+                console.error(error);
+                return 'Uh oh! Something went wrong.';
+            });
+    }
+}
+Scratch.extensions.register(new AsyncExtension());
--- main.py
+++ main.py
@@ -13,7 +13,21 @@ def generateText():
         client = Client()
         response = client.chat.completions.create(
             model="gpt-4o",
-            messages=[{"role": "system", "content": "You are a helpful assistant. You only reply in 70 tokens or less. That's 300 characters or less."}, {"role": "user", "content": prompt}],
+            messages=[{"role": "system", "content": "You are a helpful assistant. You only reply in 70 tokens or less. That's 300 characters or less. Your name is Launchpad AI."}, {"role": "user", "content": prompt}],
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        print(e)
+        return jsonify({"success": False, "data": str(e)})
+
+@app.route("/wm")
+def generateTextWM():
+    try:
+        prompt = request.args.get("prompt")
+        client = Client()
+        response = client.chat.completions.create(
+            model=request.args.get("model"),
+            messages=[{"role": "system", "content": "You are a helpful assistant. You only reply in 70 tokens or less. That's 300 characters or less. Your name is Launchpad AI."}, {"role": "user", "content": prompt}],
         )
         return response.choices[0].message.content
     except Exception as e:
